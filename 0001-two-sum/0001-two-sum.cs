a22da021ce4049ec61af31d3dbddb0a0fb73b3fc
Subhiksha Ganesan <105779283+SubhikshaGanesan10@users.noreply.github.com>
2025-02-27 23:49:25 -0600

    Update 0001-two-sum.cs

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,13 +1,43 @@
-public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
-         var dict = new Dictionary<int, int>();
-        for (int i = 0; ; ++i) {
-            int x = nums[i];
-            int y = target - x;
-            if (dict.ContainsKey(y)) {
-                return new int[] { dict[y], i };
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array:");
+            int[] nums = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            Console.WriteLine("Enter the target value:");
+            int target = int.Parse(Console.ReadLine());
+
+            int[] result = TwoSum(nums, target);
+
+            Console.WriteLine("Indices: " + string.Join(", ", result));
+            Console.ReadLine();
+
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; ; i++)
+            {
+                int x = nums[i];
+                int y = target - x;
+                if (dict.ContainsKey(y))
+                {
+                    return new int[] { dict[y], i };
+                }
+                dict[x] = i;
+
             }
-            dict[x] = i;
+
         }
     }
 }
